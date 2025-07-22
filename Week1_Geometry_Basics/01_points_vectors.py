fad27c72315c84e68b3a2b2a9d02d365dd092da9
BSH <96364437+969flash@users.noreply.github.com>
2025-07-22 20:33:41 +0900

    week1_progress

--- Week1_Geometry_Basics/01_points_vectors.py
+++ Week1_Geometry_Basics/01_points_vectors.py
@@ -1,9 +1,5 @@
 import Rhino.Geometry as geo
-import scriptcontext as sc
-import Rhino
 
 base = geo.Point3d(0, 0, 0)
 vec = geo.Vector3d(10, 5, 3)
 moved = base + vec
-sc.doc.Objects.AddPoint(base)
-sc.doc.Objects.AddPoint(moved)
--- Week1_Geometry_Basics/02_curves_surfaces.py
+++ Week1_Geometry_Basics/02_curves_surfaces.py
@@ -1,10 +1,7 @@
 import Rhino.Geometry as geo
-import scriptcontext as sc
 
-pts = [geo.Point3d(i, i*i*0.1, 0) for i in range(-5, 6)]
+pts = [geo.Point3d(i, i * i * 0.1, 0) for i in range(-5, 6)]
 curve = geo.NurbsCurve.Create(False, 3, pts)
-sc.doc.Objects.AddCurve(curve)
 
-rail = geo.Line(geo.Point3d(0,0,0), geo.Point3d(0,0,10)).ToNurbsCurve()
-surface = geo.Surface.CreateExtrusion(curve, geo.Vector3d(0,0,5))
-sc.doc.Objects.AddSurface(surface)
+rail = geo.Line(geo.Point3d(0, 0, 0), geo.Point3d(0, 0, 10)).ToNurbsCurve()
+surface = geo.Surface.CreateExtrusion(curve, geo.Vector3d(0, 0, 5))
